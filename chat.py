585c439a230d75563b531daf15e3f2c707cd9518
ThatHypedPerson <60367754+ThatHypedPerson@users.noreply.github.com>
2022-04-15 21:40:37 -0700

    Added comments to chat.py

--- chat.py
+++ chat.py
@@ -1,12 +1,16 @@
 import threading
 
+# Import external files + initialize their connections
 import youtube
 import twitch
 
+# Get the YouTube livestream's chat
 youtube.init()
 
+# Set both chats to use threads
 yt_chat = threading.Thread(target=youtube.messages)
 ttv_chat = threading.Thread(target=twitch.messages)
 
+# Start reading and printing from both chats
 yt_chat.start()
 ttv_chat.start()
