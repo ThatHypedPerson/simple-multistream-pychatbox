53885dd42a21fb04c01602647be4b32509dea778
ThatHypedPerson <60367754+ThatHypedPerson@users.noreply.github.com>
2022-04-16 17:14:27 -0700

    Add user roles to Twitch Twitch chat is so much more complicated. I didn't even add all the possible badges, they can be added later though.

--- twitch.py
+++ twitch.py
@@ -12,7 +12,9 @@ twitch.send(f"PASS {login['token']}\n".encode('utf-8')) # OAuth
 twitch.send(f"NICK {login['username']}\n".encode('utf-8')) # Username
 twitch.send(f"JOIN {login['channel']}\n".encode('utf-8')) # Livestream's Channel Name
 
-while not AttributeError: # Just in case the connection has any issues from previous connections(?)
+twitch.send('CAP REQ :twitch.tv/tags\n'.encode('utf-8')) # Get user badges in responses
+
+while not (AttributeError, ConnectionResetError): # Just in case the connection has any issues from previous connections(?)
     resp = twitch.recv(2048).decode('utf-8') # Check for a successful connection
 print(f"🟪 Twitch: Connected to channel: {login['channel'][1:]}")
 
@@ -27,9 +29,25 @@ def messages():
             elif resp.startswith(":tmi.twitch.tv"): # Ignore other data sent by Twitch
                 pass
             elif resp != "" and "PRIVMSG" in resp:
-                username = resp[1 : resp.find("!")]
-                message = resp[resp.find(" :") + 2 : -2]
-                print(f"🟪 {username}: {message}")
+                # Add emojis before message based on chatter's status
+                identifiers = "🟪 "
+                badges = resp[resp.find("badges=") + 7 : resp.find(";", resp.find("badges=") + 1)]
+                if(badges != ""):
+                    if("broadcaster" in badges):
+                        identifiers += "👑 "
+                    if any(admin in badges for admin in ["admin", "global_mod", "staff"]): # no clue if these are correct
+                        identifiers += "🛠️ "
+                    if("moderator" in badges):
+                        identifiers += "⚔️ "
+                    if("vip" in badges):
+                        identifiers += "💎 "
+                    if("subscriber" in badges): # Maybe add different color stars based on subscriber status
+                        identifiers += "⭐ "
+                
+                # Filter out someone's display name and message (dumb way of doing it, but it works)
+                username = resp[resp.find("display-name=") + 13 : resp.find(";", resp.find("display-name="))]
+                message = resp[resp.find(" :", resp.find("PRIVMSG")) + 2 : -2]
+                print(f"{identifiers}{username}: {message}")
     except (SystemExit, KeyboardInterrupt): # Only stops on next message recieved
         twitch.close()
         print("\n🟪 Program exited.")
